96a2ee64e3d52ebb001ba3752a18ec37846d3c16
Joah Gerstenberg <joahd@gbergfamily.com>
2014-08-01 11:19:55 -0500

    functional

--- pentago.js
+++ pentago.js
@@ -82,9 +82,13 @@ var Pentago = function() {
     var space = this.board.space_at(coor);
     if (!space.is_occupied && col == this.turn) {
       space.is_occupied = col;
-      this.turn = this.turn == 'white' ? 'black' : 'white';
+      this.turn = this.turn == 'red' ? 'black' : 'red';
 
       this.board.rotate_square(rot['sq'], rot['dir']);
+
+      if (this.has_won(col)) {
+        alert(col + ' won!');
+      }
 
       return true;
     }
@@ -92,6 +96,62 @@ var Pentago = function() {
     return false;
   }
 
-  this.turn = 'white';
+  this.has_won = function(col) {
+    var spaces = this.board.spaces.filter(function(el) { return el.is_occupied == col });
+    var pws = [
+      'a1 a2 a3 b1 b2',
+      'a2 a3 b1 b2 b3',
+      'a4 a5 a6 b4 b5',
+      'a5 a6 b4 b5 b6',
+      'a7 a8 a9 b7 b8',
+      'a8 a9 b7 b8 b9',
+      'c1 c2 c3 d1 d2',
+      'c2 c3 d1 d2 d3',
+      'c4 c5 c6 d4 d5',
+      'c5 c6 d4 d5 d6',
+      'c7 c8 c9 d7 d8',
+      'c8 c9 d7 d8 d9',
+      'a1 a4 a7 c1 c4',
+      'a4 a7 c1 c4 c7',
+      'a2 a5 a8 c2 c5',
+      'a5 a8 c2 c5 c8',
+      'a3 a6 a9 c3 c6',
+      'a6 a9 c3 c6 c9',
+      'b1 b4 b7 d1 d4',
+      'b4 b7 d1 d4 d7',
+      'b2 b5 b8 d2 d5',
+      'b5 b8 d2 d5 d8',
+      'b3 b6 b9 d3 d6',
+      'b6 b9 d3 d6 d9',
+      'a1 a5 a9 d1 d5',
+      'a5 a9 d1 d5 d9',
+      'b3 b5 b7 c3 c5',
+      'b5 b7 c3 c5 c7',
+      'a2 a6 b7 d2 d6',
+      'a4 a8 c3 d4 d8',
+      'b2 b4 a9 c2 c4',
+      'b6 b8 d1 c6 c8'
+    ]
+
+    for (var i = 0; i < pws.length; i += 1) {
+      pw = pws[i].split(' ');
+      var t = true;
+
+      for (var j = 0; j < pw.length; j += 1) {
+        if (spaces.filter(function(el) { return el.loc == pw[j] }).length == 0) {
+          t = false;
+          break;
+        }
+      }
+
+      if (t) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  this.turn = 'red';
   this.board = new Board();
 }
--- script.js
+++ script.js
@@ -9,28 +9,67 @@ $(document).ready(function() {
 
   refresh = function() {
     $('#pentago').html('');
+    $('.move').text(pentago.turn[0].toUpperCase() + pentago.turn.substr(1, pentago.turn.length-1) + ' to move.')
 
-    var order = ['a1 a2 a3 b1 b2 b3',
-                 'a4 a5 a6 b4 b5 b6',
-                 'a7 a8 a9 b7 b8 b9',
-                 'c1 c2 c3 d1 d2 d3',
-                 'c4 c5 c6 d4 d5 d6',
-                 'c7 c8 c9 d7 d8 d9']
+    var order = [
+      [
+        'a1 a2 a3', 
+        'a4 a5 a6',
+        'a7 a8 a9'
+      ],
+      [
+        'b1 b2 b3',
+        'b4 b5 b6',
+        'b7 b8 b9'
+      ],
+      [
+        'c1 c2 c3',
+        'c4 c5 c6',
+        'c7 c8 c9'
+      ],
+      [
+        'd1 d2 d3',
+        'd4 d5 d6',
+        'd7 d8 d9'
+      ]
+    ]
 
     for (var i = 0; i < order.length; i += 1) {
-      var row = order[i].split(' ');
       var html = '';
-      html += '<div class="row">';
+      html += '<div class="block">';
 
-      for (var j = 0; j < row.length; j += 1) {
-        var space = pentago.board.space_at(row[j]);
+      for (var j = 0; j < order[i].length; j += 1) {
+        html += '<div class="row">';
+        var row = order[i][j].split(' ');
 
-        html += '<div class="space' + (space.is_occupied ? ' ' + space.is_occupied : '') + '" id="' + space.loc + '"></div>';
+        for (var k = 0; k < row.length; k += 1) {
+          var space = pentago.board.space_at(row[k]);
+
+          html += '<div class="space' + (space.is_occupied ? ' ' + space.is_occupied : '') + '" id="' + space.loc + '"></div>';
+        }
+
+        html += '</div>';
       }
 
       html += '</div>';
+
       $('#pentago').append(html);
     }
+
+    // for (var i = 0; i < order.length; i += 1) {
+    //   var row = order[i].split(' ');
+    //   var html = '';
+    //   html += '<div class="row">';
+
+    //   for (var j = 0; j < row.length; j += 1) {
+    //     var space = pentago.board.space_at(row[j]);
+
+    //     html += '<div class="space' + (space.is_occupied ? ' ' + space.is_occupied : '') + '" id="' + space.loc + '"></div>';
+    //   }
+
+    //   html += '</div>';
+    //   $('#pentago').append(html);
+    // }
   }
 
   reset();
@@ -41,9 +80,11 @@ $(document).ready(function() {
       var space = this.id;
       $(this) .addClass(pentago.turn)
       $(document).off('click', '#pentago .space')
+
+      $('.move').text(pentago.turn[0].toUpperCase() + pentago.turn.substr(1, pentago.turn.length-1) + ' to rotate.')
       $(document).on('click', '#pentago .space', function() {
         if (parseInt(this.id[1], 10) % 3 == 1 || parseInt(this.id[1], 10) % 3 == 0) {
-        pentago.place_piece(pentago.turn, space, { sq: this.id[0], dir: (parseInt(this.id[1], 10) % 3 == 1 ? 'ccw' : 'cw'  });
+        pentago.place_piece(pentago.turn, space, { sq: this.id[0], dir: (parseInt(this.id[1], 10) % 3 == 1 ? 'ccw' : 'cw')  });
         refresh();
         $(document).off('click', '#pentago .space')
         $(document).on('click', '#pentago .space', click_helper)
